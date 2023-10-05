c4c2b7627db3d600e2b82544dff7eef32737233c
TheWarley1 <84907914+TheWarley1@users.noreply.github.com>
2023-10-05 14:21:50 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(<div><h1>hello</h1> <p>can fit in multiple elements ina div</p></div>, document.getElementById("root"));
